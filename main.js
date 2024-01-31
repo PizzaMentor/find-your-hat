c659880be113d855cd0732fcf70e51cb53bcd8d6
PizzaMentor <pizzzamentor@gmail.com>
2024-01-31 01:12:03 +0000

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PizzaMentor/find-your-hat.git
+git push -u origin main
